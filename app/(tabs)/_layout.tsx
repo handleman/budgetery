7b1fac1da25ffb4b5ab95fa52f700df487e2737a
handleman <pavel.handleman@gmail.com>
2024-07-13 21:49:07 +0300

    typo

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -15,7 +15,7 @@ export default function TabLayout() {
         headerShown: false,
       }}>
       <Tabs.Screen
-        name="index"
+        name="income"
         options={{
           title: 'Income',
           tabBarIcon: ({ color, focused }) => (
